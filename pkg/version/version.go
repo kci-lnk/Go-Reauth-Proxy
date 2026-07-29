3a9aed7a455fa47a608c54c08eff04637d7818bb
kci-lnk <taipeiami@icloud.com>
2026-07-29 12:15:43 +0800

    chore(release): bump version to 2.1.2

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -3,7 +3,7 @@ package version
 // Version and Commit are variables so release builds can inject the exact
 // bundle identity with -ldflags -X. Development builds retain useful values.
 var (
-	Version = "2.1.1"
+	Version = "2.1.2"
 	Commit  = "unknown"
 )
 

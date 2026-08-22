9414edf906fbc2cd87f5aa792cfcaacd803dc581
kci-lnk <taipeiami@icloud.com>
2026-08-23 04:13:50 +0800

    chore(release): prepare v2.3.4

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -3,6 +3,6 @@ package version
 // Version and Commit are variables so release builds can inject the exact
 // bundle identity with -ldflags -X. Development builds retain useful values.
 var (
-	Version = "2.3.3"
+	Version = "2.3.4"
 	Commit  = "unknown"
 )
